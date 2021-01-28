c0ea1f0332d2f57d80e6dd6885e5638e80d059b5
Connor Curtis <concur1998@gmail.com>
2021-01-27 16:13:56 -0800

    added some comments and erased some things in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+//Declaring global variables...
 var products = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.jpg', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg']
 
 var imageContainer = document.getElementById('image-container')
@@ -9,6 +9,11 @@ var centerImageElement = document.getElementById('center-product')
 var rightImageElement = document.getElementById('right-product')
 var roundsOfVoting = 25;
 
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var votesByProduct = [];
+var timesProductsAreShown = [];
+//Creating a function that takes images, the name of the image, the times shown, and times clicked. Then it pushes this data into allImages and imageMap.
 function ProductImage(name) {
   this.name = name.substring(0, name.length - 4);
   this.timesShown = 0;
@@ -26,12 +31,12 @@ ProductImage.imageMap = {};
 for (var i = 0; i < products.length; i++) {
   new ProductImage(products[i]);
 }
-
+//Generator that puts 3 random images out of the ones listed onto the screen.
 function generateRandomImages() {
   var leftIndex = Math.floor(Math.random() * ProductImage.allImages.length);
   var rightIndex = Math.floor(Math.random() * ProductImage.allImages.length);
   var centerIndex = Math.floor(Math.random() * ProductImage.allImages.length);
-
+//This makes sure we don't get duplicates.
   while (leftIndex === rightIndex || leftIndex === centerIndex) {
     if (leftIndex === rightIndex) {
       rightIndex = Math.floor(Math.random() * ProductImage.allImages.length);
@@ -52,7 +57,7 @@ function renderImages() {
 
   var newImages = generateRandomImages();
 
-  while(
+  while (
     currentlyRenderedImages[0] === newImages[0].name ||
     currentlyRenderedImages[1] === newImages[0].name ||
     currentlyRenderedImages[2] === newImages[0].name ||
@@ -72,50 +77,47 @@ function renderImages() {
     currentlyRenderedImages[1] === newImages[2].name ||
     currentlyRenderedImages[2] === newImages[2].name ||
     currentlyRenderedImages[0] === newImages[0].name ||
-    currentlyRenderedImages[1] === newImages[0].name 
+    currentlyRenderedImages[1] === newImages[0].name
   ) {
     newImages = generateRandomImages();
   }
   leftImageElement.src = newImages[0].image;
   leftImageElement.name = newImages[0].name;
-newImages[0].timesShown++;
+  newImages[0].timesShown++;
 
   centerImageElement.src = newImages[1].image;
   centerImageElement.name = newImages[1].name;
-newImages[1].timesShown++;
+  newImages[1].timesShown++;
 
   rightImageElement.src = newImages[2].image;
   rightImageElement.name = newImages[2].name;
-newImages[2].timesShown++;
+  newImages[2].timesShown++;
 }
-//   for (var i = 0; i < newImages.length; i++)  {
-//     for ()
-//     if (newImages[i].name === currentlyRenderedImages
-//   }
-// }
+
+
 
 function handleImageClick(event) {
 
   roundsOfVoting--;
 
-  for (var i = 0; i< ProductImage.allImages.length; i++)  {
+  for (var i = 0; i < ProductImage.allImages.length; i++) {
     if (event.target.name === ProductImage.allImages[i].name) {
       ProductImage.allImages[i].timesClicked++;
     }
   }
-renderImages();
-if (!roundsOfVoting)  {
-  imageContainer.removeEventListener('click', handleImageClick);
-  renderResults();
-}
+  renderImages();
+  if (!roundsOfVoting) {
+    imageContainer.removeEventListener('click', handleImageClick);
+    renderResults();
+  }
 }
 
 renderImages();
 imageContainer.addEventListener('click', handleImageClick);
 
-function renderResults()  {
+function renderResults() {
   var imageInfo = document.getElementById('image-info')
-  for (var i = 0; i < ProductImage.allImages.length; i++){
+  for (var i = 0; i < ProductImage.allImages.length; i++) {
     var rowElement = document.createElement('tr');
     var nameElement = document.createElement('td');
     nameElement.textContent = ProductImage.allImages[i].name + ProductImage.allImages[i].timesClicked + ProductImage.allImages[i].timesShown;
@@ -125,57 +127,39 @@ function renderResults()  {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-// function ProductImage(image)  {
-//   this.timesClicked = 0;
-//   this.timesShown = 0;
-//   this.image = image;
-//   this.product = product;
-
-//   ProductImage.allImages.push(this);
+// for (var i = 0; i < ProductImage.allImages.length; i++) {
+//   votesByProduct.push(ProductImage.allImages[i].timesClicked);
+//   timesProductsAreShown.push(ProductImage.allImages[i].timesShown);
 // }
 
-// ProductImage.allImages = [];
 
-// new ProductImage('images/bag.jpg');
-// new ProductImage('images/banana.jpg');
-// new ProductImage('images/bathroom.jpg');
-// new ProductImage('images/boots.jpg');
-// new ProductImage('images/breakfast.jpg');
-// new ProductImage('images/bubblegum.jpg');
-// new ProductImage('images/chair.jpg');
-// new ProductImage('images/cthulhu.jpg');
-// new ProductImage('images/dog-duck.jpg');
-// new ProductImage('images/dragon.jpg');
-// new ProductImage('images/pen.jpg');
-// new ProductImage('images/pet-sweep');
-// new ProductImage('images/scissors.jpg');
-// new ProductImage('images/shark.jpg');
-// new ProductImage('images/sweep.jpg');
-// new ProductImage('images/tauntaun.jpg');
-// new ProductImage('images/unicorn.jpg');
-// new ProductImage('images/usb.gif');
-// new ProductImage('images/water-can.jpg')
-// new ProductImage('images/wine-glass.jpg')
-
-
-// var productContainer = document.getElementById('product-images');
-// var leftProduct = document.getElementById('left-product');
-// var centerProduct = document.getElementById('center-product');
-// var rightProduct = document.getElementById('right-product');
-
-
-// function generateRandomProducts() {
-//   var leftIndex = Math.floor(Math.random() * ProductImage.allImages.length);
-//   var
-// }
+// var myChart = new Chart(ctx, {
+//   type: 'bar',
+//   data: {
+//     labels: products,
+//     datasets: [{
+//       label: 'times clicked',
+//       //data: [15, 50, 3, 5, 2, 3],
+//       data: timesProductsAreShown,
+//       backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+//       borderColor: ['rgba(255, 99, 132, 1)'],
+//       borderWidth: 1
+//     },
+//     {
+//       label: 'times Shows',
+//       data: [30, 100, 3, 5, 2, 3],
+//       backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+//       borderColor: ['rgba(255, 99, 132, 1)'],
+//       borderWidth: 1
+//     }]
+//   },
+//   options: {
+//     scales: {
+//       yAxes: [{
+//         ticks: {
+//           beginAtZero: true
+//         }
+//       }]
+//     }
+//   }
+// });
